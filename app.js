9c2cbba196d8b9a8059070d09702a0fc9ac7ff85
FacuM-hub <facundomartin21@gmail.com>
2021-06-08 01:34:19 +0200

    mejoré algunos aspectos del responsive en carrito

--- app.js
+++ app.js
@@ -5,29 +5,25 @@ const path = require("path");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-    const pathHtml = path.join (__dirname, "/views/index.html")
-    res.sendFile(pathHtml)
-});
-app.get("/carrito", (req, res) => {
-    const pathHtml = path.join (__dirname, "/views/carrito.html")
-    res.sendFile(pathHtml)
-});
+app.get("/", (req, res) => 
+    res.sendFile( path.join (__dirname, "/views/index.html"))
+    );
 
-app.get("/login", (req, res) => {
-    const pathHtml = path.join (__dirname, "/views/login.html")
-    res.sendFile(pathHtml)
-});
+app.get("/views/carrito", (req, res) => 
+    res.sendFile( path.join (__dirname, "/views/carrito.html"))
+    );
 
-app.get("/registro", (req, res) => {
-    const pathHtml = path.join (__dirname, "/views/registro.html")
-    res.sendFile(pathHtml)
-});
+app.get("/views/login", (req, res) => 
+    res.sendFile(path.join (__dirname, "/views/login.html"))
+);
 
-app.get("/producto", (req, res) => {
-    const pathHtml = path.join (__dirname, "/views/producto.html")
-    res.sendFile(pathHtml)
-});
+app.get("/views/registro", (req, res) => 
+    res.sendFile (path.join (__dirname, "/views/registro.html"))
+);
+
+app.get("/views/producto", (req, res) => 
+    res.sendFile( path.join (__dirname, "/views/producto.html")))
+;
 
 app.listen(3002, () =>{
     console.log("servidor corriendo")
