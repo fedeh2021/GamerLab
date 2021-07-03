dfe3b452051016d7cc10480f70c32e18d2e50daa
FacuM-hub <facundomartin21@gmail.com>
2021-07-03 23:55:03 +0200

    ruta index arreglada

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 const mainController = 
 {
     index: (req, res) => {
-        res.render("index")
+        res.render("index", {productos:products})
     }
 };
 
