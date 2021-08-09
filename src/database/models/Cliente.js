1171f42d025e203c0e9709eb61270881bbd96b6d
fedeh2021 <fedejhirsch@gmail.com>
2021-08-09 18:39:33 -0300

    cree models

---
+++ src/database/models/Cliente.js
@@ -0,0 +1,70 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = 'clientes'
+    const cols = {
+        id: {
+            type: dataTypes.SMALLINT(6).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        envioFK: {
+            type: dataTypes.INT(11),
+            allowNull:false
+        },
+        nombre: {
+            type: dataTypes.VARCHAR(123),
+            allowNull: false
+        },
+        apellido: {
+            type: dataTypes.VARCHAR(255),
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.VARCHAR(255),
+            allowNull: false
+        },
+        contraseña: {
+            type: dataTypes.VARCHAR(255),
+            allowNull: false
+        },
+        rol: {
+            type: dataTypes.TINYINT(1),
+            allowNull: false
+        },
+        dni: {
+            type: dataTypes.INT(11),
+            allowNull: false
+        },
+        fecha_nacimiento: {
+            type: dataTypes.DATE,
+            allowNull: false
+        },
+        telefono: {
+            type: dataTypes.INT(11),
+            allowNull: false
+        },
+        imagen: {
+            type: dataTypes.VARCHAR(255),
+            allowNull: false
+        },
+        created_at: {
+            type: dataTypes.DATETIME, 
+            allowNull: false
+        },
+        updated_at: {
+            type: dataTypes.DATETIME,
+            allowNull: false
+        },
+        deleted_at: {
+            type: dataTypes.DATETIME,
+            allowNull: false
+        }
+    }
+    const config = {
+        tablename: 'clientes',
+        timestamps: false,
+        camelCase: false
+    }
+    const Cliente = sequelize.define(alias, cols, config);
+
+    return Cliente;
+}
