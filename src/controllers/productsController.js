5b3bbe223b188fd0283fb025f9f6629b5e260584
fedeh2021 <fedejhirsch@gmail.com>
2021-08-10 19:05:06 -0300

    detalles en controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const { inflateRaw } = require('zlib');
-
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const db =require ("../database/models")
