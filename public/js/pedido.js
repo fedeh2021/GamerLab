9e6741d6acc182aecb2af214d42a0fbb6b0cb2f2
fedeh2021 <fedejhirsch@gmail.com>
2021-08-23 16:00:42 -0300

    carrito

--- public/js/pedido.js
+++ public/js/pedido.js
@@ -5,7 +5,7 @@ const listaProductos = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 const procesarPedidoBtn = document.getElementById('procesar-pedido');
 
-//cargarEventos();
+cargarEventos();
 
 function cargarEventos(){
 
